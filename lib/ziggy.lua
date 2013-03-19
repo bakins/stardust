b430a6fc4da895b42dadde7d20b0cbc07a454fd3
bakins <brian.akins@turner.com>
2013-03-19 10:28:00 -0400

    seperate out router into middleware

--- lib/ziggy.lua
+++ lib/ziggy.lua
@@ -15,77 +15,33 @@ local response = require "ziggy.response"
 
 local _M = {}
 
+local function dummy_last_middleware(req, res, nxt)
+end
+
 --- Create a new ziggy application.
 -- @return a ziggy application
 function _M.new()
     local self = {
-	routes = { 
-	    GET = {},
-	    POST = {},
-	    PUT = {},
-	    DELETE = {}
+	middleware = { 
+	    dummy_last_middleware
 	}
     }
     return setmetatable(self, { __index = _M })
 end
 
---- Add a route
+--- Add middleware to the stack. Middleware is called in the order it is added. While it is technically possible to add middleware 
+-- after the application has started (ie, calling run), this is not supported and may lead to strange results.
+-- Also add middleware before adding routes
 -- @param self ziggy application
--- @param method HTTP method, ie GET, POST
--- @param pattern uri pattern to match
--- @param func function to call when this pattern is matched. fucntion should take 2 arguments: a request object and a response object
--- @usage app = ziggy.new()
---app:route('GET', '/foo', function(req, res) res.body = "hello" end)
-
-function _M.route(self, method, pattern, func)
-    local t = self.routes[method]
-    if not t then
-	return nil, "unhandled method: " .. method
-    end
-    -- should we test the pattern??
-    
-    insert(t, { pattern = pattern, func = func })
-    return true, nil
-end
-
-local route = _M.route
-
--- be explicit for documentation...
-
---- Convenience function to add a route for GET
--- @param self ziggy application
--- @param pattern uri pattern to match
--- @param func function
--- @see route
-function _M.get(self, pattern, func) 
-    return route(self, "GET", pattern, func) 
-end
-
---- Convenience function to add a route for POST
--- @param self ziggy application
--- @param pattern uri pattern to match
--- @param func function
--- @see route
-function _M.post(self, pattern, func) 
-    return route(self, "POST", pattern, func) 
-end
-
---- Convenience function to add a route for PUT
--- @param self ziggy application
--- @param pattern uri pattern to match
--- @param func function
--- @see route
-function _M.put(self, pattern, func) 
-    return route(self, "PUT", pattern, func) 
-end
-
---- Convenience function to add a route for DELETE
--- @param self ziggy application
--- @param pattern uri pattern to match
--- @param func function
--- @see route
-function _M.delete(self, pattern, func) 
-    return route(self, "DELETE", pattern, func) 
+-- @param func function to call
+function _M.use(self, func) 
+    -- this is horrible, but makes sure our dummy function stays last
+    local t = self.middleware
+    local size = #t
+    local dummy = t[size]
+    t[size] = func
+    t[size+1] = dummy
+    return self
 end
 
 -- lifted from, LSD, ouzo, etc
@@ -120,26 +76,17 @@ end
 local request_new = request.new
 local response_new = response.new
 
---- Runt the application
+--- Run the application
 -- @param self ziggy application
 -- @param ngx magix nginx Lua object
 -- @usage Add something like this to nginx.conf:
 --content_by_lua 'return require("my.ziggy.module").run(ngx)';
 function _M.run(self, ngx)
-    local method = ngx.req.get_method()
-    local routes = self.routes[method]
-    if not routes then
-	return nil, "unhandled method: " .. method
-    end
-    for _,item in ipairs(routes) do
-	if find(ngx.var.uri, item.pattern) then
-	    -- we may want to provide a "wrapped" version of the request??
-	    local req, res = request_new(ngx), response_new(ngx)
-	    -- should wrap in pcall??
-	    item.func(req, res)
-	    return send_response(ngx, res)
-	end
-    end
+    local req, res = request_new(ngx), response_new(ngx)
+    local middleware = self.middleware
+    local func = middleware[1]
+    func(req, res, middleware[2] )
+    return send_response(ngx, res)
 end
 
 return _M
