35ad41143136b6deae06d89a545051e53237ee8b
bakins <brian.akins@turner.com>
2013-03-19 10:38:13 -0400

    very simple middleware example

---
+++ examples/middleware.lua
@@ -0,0 +1,30 @@
+local upper = string.upper
+local ziggy = require "ziggy"
+local cjson = require "cjson"
+local router = require "ziggy.router"
+
+local _M = {}
+
+-- stupid simple middleware that just uppercases response
+
+local function middleware(req, res, nxt)
+    res.body = upper(res.body)
+end
+
+local app = ziggy.new()
+local r = router.new()
+app:use(r)
+app:use(middleware)
+
+r:get("/", 
+      function(req, res)
+	  res.body = req.path .. "\n"
+      end
+     )
+
+function _M.run(ngx)
+    return app:run(ngx)
+end
+
+
+return _M
