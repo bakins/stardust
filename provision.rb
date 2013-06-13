66075265e19b9025da61168f962ea12f4dedbe30
bakins <brian.akins@turner.com>
2013-06-13 08:56:38 -0400

    for testing on a vm

---
+++ provision.rb
@@ -0,0 +1,25 @@
+%w[ build-essential wget libpcre3-dev luarocks git-core cpanminus curl ].each do |p|
+  package p
+end
+execute "cpanm install Test::Nginx" do
+  not_if "perl -mTest::Nginx -e'1'"
+end
+
+openresty_version = "1.2.8.5"
+
+remote_file "/home/vagrant/ngx_openresty-#{openresty_version}.tar.gz" do
+  source "http://openresty.org/download/ngx_openresty-#{openresty_version}.tar.gz"
+  notifies :run, "execute[install openresty]"
+end
+
+execute "install openresty" do
+  cwd "/home/vagrant"
+  command <<EOF
+ tar -zxvf ngx_openresty-#{openresty_version}.tar.gz
+cd ngx_openresty-#{openresty_version}
+./configure --with-luajit --prefix=/usr/local --sbin-path=/usr/local/sbin/nginx
+make
+make install
+EOF
+  action :nothing
+end
